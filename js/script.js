215b410ba7bfa17ea73c89b1d6876c36d7ba8a39
Branchertefa <105944100+Branchertefa@users.noreply.github.com>
2024-08-02 10:33:42 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-import { aleatorio } from './aleatorio.js';
-import { perguntas } from './perguntas.js';
+import {aleatorio} from './aleatorio.js';
+import {perguntas} from './perguntas.js';
 
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-const botaoJogarNovamente = document.querySelector(".novamente.btn");
-
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
 let atual = 0;
 let perguntaAtual;
@@ -24,8 +23,8 @@ function mostraPergunta() {
     mostraAlternativas();
 }
 
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas) {
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
@@ -45,14 +44,14 @@ function mostraResultado() {
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
     caixaResultado.classList.add("mostrar");
-    botaoJogarNovamente.addEventListener("click", JogarNovamente);
+    botaoJogarNovamente.addEventListener("click", jogaNovamente);
 }
 
-function botaoJogarNovamente(){
-  atual = 0;
-  historiaFinal = "";
-  caixaResultado.classList.remove("mostrar");
-  mostraPergunta();
+function jogaNovamente(){
+    atual = 0;
+    historiaFinal = "";
+    caixaResultado.classList.remove("mostrar");
+    mostraPergunta();
 }
 
 mostraPergunta();
