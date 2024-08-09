fdf3cdc866c555b14cc6579d8826864f1c5f4579
Branchertefa <105944100+Branchertefa@users.noreply.github.com>
2024-08-09 10:59:09 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,5 +1,7 @@
 const nomes = ["Estéfany", "Ana", "Adrieli", "Aime", "Emilli"]
+
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
     return lista[posicao];
+    
 }
