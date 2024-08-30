a5fef71cc1d3a0dd4696ec057e5d66158413aac9
Branchertefa <105944100+Branchertefa@users.noreply.github.com>
2024-08-30 08:03:32 -0300

    Update perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,173 +1,71 @@
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Estudar é um procedimento que exige disciplina, rigor, sentido de dever e responsabilidade. O aluno deve perceber significado no ato de estudar, reconhecer que o estudo irá lhe trazer um benefício, ou seja, estudar vale a pena. Você considera que estudar é importante?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: [
-                    "No início ficou com medo do que essa tecnologia pode fazer.",
-                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
-                ],
-                proxima: 1,
+                texto: "sim, acho que estudar é importante!",
+                afirmacao: "Estudar ajuda a abrir a mente, a superar limitações e a desenvolver uma visão mais flexível. Além disso, ajuda a aprimorar habilidades, como capacidade de resolução de problemas, disciplina e responsabilidade.",
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: [
-                    "Quis saber como usar IA no seu dia a dia.",
-                    "Pensou que IA pode ajudar em tarefas da sua vida."
-                ],
-                proxima: 2,
+                texto: "Não, acho uma perda de tempo.",
+                afirmacao: "Sinto muito que pense assim, pois os estudos são importantes. Com eles adquirimos conhecimentos, cultura, e traçamos objetivos na vida.",
             },
-        ]
+        ],
+        proximo: 1,
     },
     {
-        enunciado: "Utilizar uma IA pode ser aterrorizante mesmo, e foi pensando nisso que uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Atualmente somente 4.3% dos municípios do Paraná tem Sistema Próprio de Ensino,  95.7% de seus municípios estão vinculados ao sistema estadual de ensino, no qual se compreende que escolas públicas e particulares, devem seguir as diretrizes do Conselho Estadual de Educação.  Você está satisfeito com o método de ensino?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: [
-                    "Conseguiu utilizar a IA para buscar informações úteis.",
-                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
-                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
-                ],
-                proxima: 3,
+                texto: "Sim, estou satisfeito com o ensino.",
+                afirmacao: "Que bom! O ensino oferece educação de qualidade, formando cidadãos com amplos conhecimentos humanísticos, científicos e tecnológicos , que potencializam suas competências pessoais e profissionais mediante os desafios do mundo do trabalho e do exercício efetivo da cidadania.",
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: [
-                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
-                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
-                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
-                ],
-                proxima: 4,
+                texto: "Não, acredito que poderia ser melhor.",
+                afirmacao: "O Sistema Estadual de Ensino existe para otimizar todas as atividades escolares e oferecer a melhor experiência ao aluno. Com uma estrutura pedagógica completa, que abrange inclusive o material didático.",
             },
-        ]
+        ],
+        proxima : 2,
     },
     {
-        enunciado: "Um chat com IA pode ser uma boa alternativa para realizar tarefas do dia a dia e foi pensando nisso que uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Estudos apontam que estudar pode aumentar a capacidade da memória, ativar o poder de concentração e, com o hábito, aumentar a nossa capacidade de aprender. Quanto mais estudamos e exercitamos o cérebro, mais protegido ele estará de doenças de degeneração, como o mal de Alzheimer, por exemplo. Você acredita que estudar faz bem para nossa saúde?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: [
-                    "Conseguiu utilizar a IA para buscar informações úteis.",
-                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
-                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
-                ],
-                proxima: 3,
+                texto: "Sim, acredito!",
+                afirmacao: "A concentração é uma habilidade fundamental que pode ser aprimorada através do hábito de estudar todos os dias. Quando você se dedica a sessões regulares de estudo, treina sua mente para se concentrar em uma única tarefa por um período prolongado.",
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: [
-                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
-                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
-                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
-                ],
-                proxima: 4,
+                texto: "Não, acho que não tem nenhuma ligação.",
+                afirmacao: "Existem muitas pesquisas em andamento sobre a relação entre a recuperação de lesões cerebrais e o nível de escolaridade do indivíduo, mas o que já sabemos é que um dos benefícios de estudar é que a reserva cognitiva de quem passou anos e anos estudando é como um cofre. Nele, você vai guardando alguns conhecimentos que serão utilizados em casos de emergência.",
             },
-        ]
+        ],
+        proximo : 3,
     },
     {
-        enunciado: "Ferramentas de busca que utilizam IA podem facilitar muito nosso processo de busca por informações. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        enunciado: "O cansaço constante proveniente de longas sessões de estudo pode levar à exaustão mental. Isso, por sua vez, pode prejudicar a capacidade de concentração e diminuir a motivação para estudar. Qual sua opinião sobre estudar muito?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
-                ],
-                proxima: 5,
+                texto: "Acho que estudar nunca é demais!",
+                afirmacao: "Estudar demais pode comprometer o seu aprendizado e até mesmo afetar a sua saúde. Ler, pesquisar e aprender obsessivamente pode fornecer uma falsa sensação de realização, sem que o conhecimento seja realmente incorporado e resulte em mais produtividade.",
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
-                ],
-                proxima: 6,
+                texto: "Acho que estudar muito ou pouco não muda.",
+                afirmacao: "É importante lembrar novamente que exagerar na quantidade de horas de estudo por dia pode causar exaustão e prejudicar seu desempenho durante a prova. Dessa forma, há uma média de quatro horas de estudo diário recomendada a fim de que você não se canse mentalmente.",
             },
-        ]
+        ],
+        proximo : 4,
     },
     {
-        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        enunciado: "Sem uma rotina de estudos, pode ficar difícil reter uma grande quantidade de conteúdos. Então, uma das principais formas de superar a dificuldade de aprender é por meio de um planejamento diário. O que você faz quando está com dificuldade na aprendizagem?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
-                ],
-                proxima: 5,
+                texto: "Eu estudo até aprender a matéria",
+                afirmacao: "O cansaço é uma das causas mais alegadas por aqueles que reclamam que não conseguem estudar. Por isso, antes de desesperar-se, você já verificou se não precisa de um tempo para respirar? Anda dormindo bem? Comeu bem? Dormiu por pelo menos 6 horas? Estar descansado é essencial para uma boa absorção sobre os conteúdos.",
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
-                ],
-                proxima: 6,
+                texto: "Eu deixo para trás e vou para o próximo assunto.",
+                afirmacao: "Às vezes só precisamos de um tempo para assimilar, passar para um próximo assunto em que tenho mais afinidade talvez ajude. E depois, com mais tempo, mais descansado e mais paciencioso consiga aprender melhor o conteúdo.",
             },
-        ]
+        ],
     },
-    {
-        enunciado: "Novas tecnologias trazem novos desafios, por isso após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
-        alternativas: [
-            {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
-                ],
-                proxima: 7,
-            },
-            {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
-                ],
-                proxima: 7,
-            },
-        ]
-    },
-    {
-        enunciado: "É muito importante refletir sobre os trabalhadores do futuro. Por isso a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
-        alternativas: [
-            {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
-                ],
-                proxima: 7,
-            },
-            {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
-                ],
-                proxima: 7,
-            },
-        ]
-    },
-    {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [
-            {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: [
-                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
-                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
-                ],
-            },
-            {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: [
-                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
-                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
-                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
-                ],
-            },
-        ]
-    }
 ];
